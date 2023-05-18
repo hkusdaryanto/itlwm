30e8c96f69932d28e68468a7773502e02add1694
Henry Kusdaryanto <henry.kusdaryanto@gmail.com>
2023-05-18 21:58:00 +0700

    add airport legacy Sierra-Lion and disable configureReport updateReport

--- include/Airport/IO80211P2PInterface.h
+++ include/Airport/IO80211P2PInterface.h
@@ -11,14 +11,14 @@ public:
 #if __IO80211_TARGET >= __MAC_11_0
     virtual bool willTerminate(IOService *,uint) APPLE_KEXT_OVERRIDE;
 #endif
-    virtual IOReturn configureReport(IOReportChannelList   *channels,
-                                     IOReportConfigureAction action,
-                                     void                  *result,
-                                     void                  *destination) APPLE_KEXT_OVERRIDE;
-    virtual IOReturn updateReport(IOReportChannelList      *channels,
-                                  IOReportUpdateAction      action,
-                                  void                     *result,
-                                  void                     *destination) APPLE_KEXT_OVERRIDE;
+    //virtual IOReturn configureReport(IOReportChannelList   *channels,
+    //                                 IOReportConfigureAction action,
+    //                                 void                  *result,
+    //                                 void                  *destination) APPLE_KEXT_OVERRIDE;
+    //virtual IOReturn updateReport(IOReportChannelList      *channels,
+    //                              IOReportUpdateAction      action,
+    //                             void                     *result,
+    //                              void                     *destination) APPLE_KEXT_OVERRIDE;
     
     virtual bool terminate( IOOptionBits options = 0 ) APPLE_KEXT_OVERRIDE;
     virtual bool attach(IOService *) APPLE_KEXT_OVERRIDE;
